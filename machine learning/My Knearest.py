76576bd15dbd35a7875010ebadc441dc53450570
Rakshith <rakshith.yn@gmail.com>
2018-09-08 11:29:39 +0530

    .

--- machine learning/My Knearest.py
+++ machine learning/My Knearest.py
@@ -1,8 +1,16 @@
 from math import sqrt
+import numpy as np
+from matplotlib import style
+from collections import Counter
+import warnings
+import matplotlib.pyplot as plt
 
-plot1 = [2,5]
-plot2 = [5,6]
 
-eculidean_distance = sqrt((plot1[0]-plot2[0])**2+(plot1[1]-plot2[1])**2)
-print(eculidean_distance)
+style.use('fivethirtyeight')
 
+dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+new_features = [5,7]
+
+[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1])
+plt.show()
